f749ffbf5e0369a37590ba81f3b9aa7eb30a49ea
SAVR04 <iamsv23@gmail.com>
2026-09-23 22:31:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (5.18%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,32 @@
 class Solution {
+    public static int[] dp=new int[31];
+    static{
+        Arrays.fill(dp,-1);
+    }
     public int fib(int n) {
         if(n==0)return 0;
         if(n==1)return 1;
-        return fib(n-1)+fib(n-2);
+
+        int first ,second;
+
+        if(dp[n-1]!=-1)
+        {
+            first=dp[n-1];
+        }
+        else
+        {
+            first=fib(n-1);
+        }
+        
+        if(dp[n-2]!=-1)
+        {
+            second=dp[n-2];
+        }
+        else
+        {
+            second=fib(n-2);
+        }
+
+        return dp[n]=first+second;
     }
 }
